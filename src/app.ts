b12b15d46bec2ffa52d923993acb0967393e1116
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-06-02 22:44:31 -0300

    docker image working

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 app.listen(3333);
 
 app.get('/', (req: any, res: any) => {
-  return res.send('Hello World!');
+  return res.send('Hello World I am inside docker and it catches random updates!');
 });	
 
 export default app;
