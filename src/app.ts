2ca8976237c9a62a7e17e2588746f7e248998bcd
Lucas Levandoski <lucas-levandoski@hotmail.com>
2022-06-03 02:52:33 +0000

    docker container working in vscode with nodemon

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ const app = express();
 
 app.listen(3333);
 
-app.get('/', (req: any, res: any) => {
-  return res.send('Hello World I am inside docker and it catches random updates!');
-});	
+app.get('/', (req: express.Request, res: express.Response) => {
+  const { test } = req.query;
+
+  return res.send('Hello World I am inside docker and it catches random updates!' + test);
+});
 
 export default app;
